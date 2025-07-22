b88ac14a0b6ffbb58a2fa087a3a8708ba4c08072
jgorostegui-veridas <jgorostegui@veridas.com>
2025-07-22 09:17:38 +0200

    Add 01-cd1898-Observing-Cloud-Resources project

---
+++ 01-cd1898-Observing-Cloud-Resources/Project/starter/terraform/_config.tf
@@ -0,0 +1,15 @@
+terraform {
+   backend "s3" {
+     bucket = "terraform-state-jgorostegui" # Replace it with your S3 bucket name
+     key    = "terraform/terraform.tfstate"
+     region = "us-east-2"  # Update to your desired region
+   }
+ }
+
+ provider "aws" {
+   region = "us-east-2"
+
+   default_tags {
+     tags = local.tags
+   }
+ }
